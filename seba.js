d23811f2c37bb9ae0131f18de4f1c4b6c19450a2
AniaPiWo <snowflake@wp.pl>
2025-02-21 17:53:46 +0100

    fix/error handling

--- seba.js
+++ seba.js
@@ -1,33 +1,54 @@
 require("dotenv").config();
+
+if (!process.env.TELEGRAM_BOT_TOKEN) {
+  console.error("❌ Brak tokena API! Sprawdź plik .env");
+  process.exit(1);
+}
+
 const { Telegraf } = require("telegraf");
+const seba = new Telegraf(process.env.TELEGRAM_BOT_TOKEN);
 
-const bot = new Telegraf(process.env.TELEGRAM_BOT_TOKEN);
+seba.telegram.getMe().then((botInfo) => {
+  console.log("✅ Seba wystartował, siema wariaty!");
+  console.log(botInfo);
+  //console.log(`Bot został zainicjalizowany jako @${botInfo.username}`);
+});
 
-bot.start((ctx) => ctx.reply("Siema byku! Co tam? 😎"));
-bot.help((ctx) =>
+const odpowiedzi = [
+  "Hehe, piwko to życie, wiadomo 🍺",
+  "Byku, jak nie piwko to co? 😎",
+  "Dobra, dobra, ale masz jakieś 2 zł na browara? 🤣",
+  "Ty, a może jakiegoś hot-doga z Orlenu ogarniemy? 🌭",
+  "Szanuję za luźną gadkę, byle nie o robocie! 💪",
+  "Poczekaj, tylko dopiję browarka i odpowiem 😂",
+];
+
+// komendy bota
+seba.start((ctx) => ctx.reply("Siema byku! Co tam? 😎"));
+seba.help((ctx) =>
   ctx.reply("No co, hehe, pytaj śmiało! Może o dobrego browarka? 🍻")
 );
-bot.on("text", (ctx) => {
-  const odpowiedzi = [
-    "Hehe, piwko to życie, wiadomo 🍺",
-    "Byku, jak nie piwko to co? 😎",
-    "Dobra, dobra, ale masz jakieś 2 zł na browara? 🤣",
-    "Ty, a może jakiegoś hot-doga z Orlenu ogarniemy? 🌭",
-    "Szanuję za luźną gadkę, byle nie o robocie! 💪",
-    "Poczekaj, tylko dopiję browarka i odpowiem 😂",
-  ];
+seba.on("text", (ctx) => {
   const randomOdp = odpowiedzi[Math.floor(Math.random() * odpowiedzi.length)];
   ctx.reply(randomOdp);
 });
 
-bot
-  .launch()
-  .then(() => {
-    console.log("Bot Seba wystartował, siema wariaty! 🍺");
-  })
-  .catch((err) => {
-    console.error("Błąd podczas uruchamiania bota:", err);
-    console.error(
-      "Sprawdź, czy token API jest poprawny i czy bot jest aktywny."
-    );
-  });
+// uruchomienie seby
+console.log("Budzimy Sebę...");
+seba.launch().catch((err) => {
+  console.error("❌ Błąd podczas uruchamiania Seby:", err);
+  console.error("Pełny błąd:", err.stack);
+  process.exit(1);
+});
+
+// bezpieczne zamykanie
+process.once("SIGINT", () => {
+  console.log("Seba idzie spać...");
+  seba.stop("SIGINT");
+  console.log("Seba poszedł spać!");
+});
+process.once("SIGTERM", () => {
+  console.log("Seba idzie spać...");
+  seba.stop("SIGTERM");
+  console.log("Seba poszedł spać!");
+});
